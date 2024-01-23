71c4f7bffe1261e546c5ff9f6ab7a856b20347c6
정석 <66814045+ljs7463@users.noreply.github.com>
2024-01-23 09:50:13 +0900

    fix

--- airflow/plugins/oauth_path.py
+++ airflow/plugins/oauth_path.py
@@ -3,4 +3,5 @@ import sys
 import os
 # GOOGLE_AUTH = '../core/config/gcp_oauth.json'
 # 환경 변수에서 GOOGLE_AUTH 값을 가져옵니다. 환경 변수가 설정되어 있지 않으면 기본 경로를 사용합니다.
-GOOGLE_AUTH = os.environ.get("BIGQUERY_AUTH_JSON_PATH", '/opt/airflow/core/config/gcp_oauth.json')
+#GOOGLE_AUTH = os.environ.get("BIGQUERY_AUTH_JSON_PATH", '/opt/airflow/core/config/gcp_oauth.json')
+GOOGLE_AUTH = "gcp_oauth.json"
